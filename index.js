7231e0e5d11810d15bc671cd93a650b788c9f573
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-16 15:32:00 +0600

    added patch and delete method

--- index.js
+++ index.js
@@ -99,6 +99,28 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/users', async(req, res) =>{
+      console.log(req.body);
+      const {email, lastSignInTime} = req.body;
+      const filter = {email: email};
+      const updatedDoc ={
+        $set:{
+          lastSignInTime: lastSignInTime
+        }
+      }
+
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+
+    })
+
+    app.delete('/users/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
